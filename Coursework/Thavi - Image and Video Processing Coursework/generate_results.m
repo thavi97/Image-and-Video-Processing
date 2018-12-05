72debcf06ca1f4c574519a9dbcd8fa98e635cb79
Thavi <thavit97@gmail.com>
2018-12-05 21:01:18 +0000

    Done!

---
+++ Coursework/Thavi - Image and Video Processing Coursework/generate_results.m
@@ -0,0 +1,25 @@
+%It will take several seconds for the output images to pop up because the
+%algorithm is being ran on four images.
+
+%Read in the image and convert to double values
+HW1 = im2double(imread('HW1.png'));
+HW4 = im2double(imread('HW4.png'));
+PR4 = im2double(imread('PR4.png'));
+PR7 = im2double(imread('PR7.png'));
+Stem_Cells = im2double(imread('Stem_Cells.png'));
+
+%If you want to select the seed manually on the image, then set the x and y coordinates to 0
+HW1_SRG = seeded_region_growing(0.2,HW1,0,0);
+HW4_SRG = seeded_region_growing(0.2,HW4,0,0);
+PR4_SRG = seeded_region_growing(0.4,PR4,0,0);
+PR7_SRG = seeded_region_growing(0.2,PR7,0,0);
+Stem_Cells_SRG = seeded_region_growing(0.1,Stem_Cells,0,0);
+
+%Show the binarised image
+figure, imshow(HW1_SRG);
+figure, imshow(HW4_SRG);
+figure, imshow(PR4_SRG);
+figure, imshow(PR7_SRG);
+figure, imshow(Stem_Cells_SRG);
+
+colormap gray(255);
